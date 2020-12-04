c4af3c9e9e6f7c171c1cfb92275869bbe2788e6d
mshaz chin <azizahsham@yahoo.com>
2020-12-04 20:39:20 +0800

    init

--- week04/scripts.js
+++ week04/scripts.js
@@ -1,8 +1,21 @@
 function transformUpperCase(){
 let a = document.getElementById("namaPengguna");
-a.value  a.value.toUpperCase();
+a.value = a.value.toUpperCase();
 
 }
 
 let elUsername = document.getElementById("namePengguna");
 elUsername.addEventListener("keyup", transformUpperCase);
+
+function checkPasswordLength() {
+
+    let password = document.getElementById("kataLaluan").value;
+    if(password.length < 5){
+        alert("Password must be more than 5 characters");
+    } else {
+        alert("Password accepted")
+    }
+}
+
+let elPassword - document.getElementById("kataLaluan");
+elPassword.onblur = checkPasswordLength;
