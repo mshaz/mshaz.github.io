c20eab345aa9d1847d631ab7750b7fd12e3800df
mshaz chin <azizahsham@yahoo.com>
2020-12-21 01:01:37 +0800

    init

--- week06/scripts/fetch.js
+++ week06/scripts/fetch.js
@@ -1,26 +1,30 @@
 function GetBookings(){
     let url = 'https://api.sheety.co/e4e1e3ba7d813589a03b055bfb0ae62f/bookingApp/bookings';
-fetch(url)
-.then((response) => response.json())
-.then(json => {
-  // Do something with the data
-  console.log(json.bookings);
-  let bookingList = document.getElementById("bookingList");
-  bookingList.innerHTML="";
-  for(let i =0; i< json.bookings.length;i++){
-      let gName = json.bookings[i].name;
-      let gEmail = json.bookings[i].email;
-      let gPax = json.bookings[i].pax;
-      let gID = json.bookings[i].id;
+    fetch(url)
+    .then((response) => response.json())
+    .then(json => {
+        // Do something with the data
+        console.log(json.bookings);
+        let bookingList = document.getElementById("bookingList");
+        bookingList.innerHTML = "";
 
-      bookingList.innerHTML += gId + "-" +gName + ","+
-          gEmail + ", pax:" + pax + "<br>";
-  }
-});
+        let jsonDiv = document.getElementById("json");
+        jsonDiv.innerHTML = "";
+        jsonDiv.innerHTML = json.bookings;
 
+        for (let i = 0; i < json.bookings.length; i++) {
+            let gName = json.bookings[i].name;
+            let gEmail = json.bookings[i].email;
+            let gPax = json.bookings[i].pax;
+            let gId = json.bookings[i].id;
+
+            bookingList.innerHTML += gId + "-" + gName + ", " +
+                gEmail + ", pax:" + gPax + "<br>";
+        }
+    });
+}
 
 let getBookingBtn = document.getElementById("getBooking");
-getBookingBtn.addEventListener("click",function(){
-    GetBookings();
-
+getBookingBtn.addEventListener("click", function () {
+GetBookings();
 });
